cca927462c2a7600855b4d23676fff424ee35264
Thomas Reinholdsson <reinholdsson@gmail.com>
2013-02-03 00:41:32 +0100

    Added several methods

--- R/hierarchy-class.r
+++ R/hierarchy-class.r
@@ -1,54 +1,57 @@
 # Path Enumeration
 
-Hierarchy <- setRefClass(
-    "Hierarchy",
+path_enum <- setRefClass(
+    "path.enumeration",
     fields = list(.data = "character", .path = "character", .sep = "character"),
     methods = list(
         initialize = function(x, y = colnames(x)[1]) {
-            
             .path <<- y  # have to be assigned before x
             .sep <<- "\\."
-            
             .data <<- deparse(substitute(x))
-            
-
         },
         
-        data = function() eval(parse(text = .data)),  # get data
+        # Get all data
+        data = function() eval(parse(text = .data)),
 
-        match = function(path, re) {
-            data()[grep(sprintf(re, path, .sep), data()[[.path]])][[.path]]
-        },
+        # Get and filter data by match
+        match = function(path, re) data()[grep(sprintf(re, path, .sep), data()[[.path]]), ][[.path]],
+        
+        # Check if path id exists in data
+        path_exists = function(path) path %in% data()[[.path]],
+        
+        # Validate path
+        validate = function(path) if (!path_exists(path)) stop("path does not exist"),
         
         # Find the position of the last seperator in path
-        last_sep_position = function(path) {
-            max(gregexpr(.sep, path)[[1]])
-        },
+        last_sep_position = function(path) max(gregexpr(.sep, path)[[1]]),
         
+        # Parent methods
         parent_id = function(path) {
-            sep_nchar <- 1  # currently only allows one character in seperator (excl. "\\")
-            substr(path, 0, last_sep_position(path) - sep_nchar)
+            validate(path)
+            x <- gsub(sprintf("(^|%s)\\w*$", .sep), "", path)
+            x <- as.character(x)
+            if (nchar(x) > 0) return(x) else return(NULL)
         },
+        parent = function(...) data()[data()[[.path]] %in% parent_id(...), ],
+        has_parent = function(path) if (is.null(parent_id(path))) FALSE else TRUE,
         
-        parent = function(...) {
-            data()[data()[[.path]] %in% parent_id(...)]
-        },
-        
+        # Descendants methods
         descendants_ids = function(path) {
-            match(path, "^%1$s%2$s\\w*(%2$s|$)")
+            validate(path)
+            x <- match(path, "^%1$s%2$s\\w*(%2$s|$)")
+            x <- as.character(x)
         },
+        descendants = function(...) data()[data()[[.path]] %in% descendants_ids(...), ],
         
-        descendants = function(...) {
-            data()[data()[[.path]] %in% descendants_ids(...)]
+        # Children methods
+        children_ids = function(path) { 
+            validate(path)
+            x <- match(path, "^%1$s%2$s\\w*$") 
+            x <- as.character(x)
+            if (length(x) > 0) return(x) else return(NULL)
         },
-        
-        children_ids = function(path) {
-            match(path, "^%1$s%2$s\\w*$")
-        },
-        
-        children = function(...) {
-            data()[data()[[.path]] %in% children_ids(...)]
-        }
+        children = function(...) data()[data()[[.path]] %in% children_ids(...), ],
+        has_children = function(path) if (is.null(children_ids(path))) FALSE else TRUE
         
     )
 )
--- inst/tests/tests.r
+++ inst/tests/tests.r
@@ -1,16 +1,19 @@
 # test
-dt <- data.table(
+dt <- data.frame(
     id = c("1", "1.1", "1.1.1", "1.1.2", "1.12", "1.12.1", "1.12.2"), 
     value = c(NA, NA, 5, 10, NA, 2, 3), 
     weight = c(NA, NA, 5333, 10, NA, 2, 3))
-a <- Hierarchy$new(dt)
-
+a <- path_enum$new(dt)
 a$data()
-a$descendants_ids("1.1")
-a$descendants("1.1")
 
-a$children_ids("1")
-a$children("1")
+x <- "1.1"
+a$descendants_ids(x)
+a$descendants(x)
 
-a$parent_id("1.1.12")
-a$parent("1.1.12")
+a$children_ids(x)
+a$children(x)
+a$has_children(x)
+
+a$parent_id(x)
+a$parent(x)
+a$has_parent(x)
