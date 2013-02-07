7e6ba03f86cfa07081d7a50ca825fd72627c4734
Thomas Reinholdsson <reinholdsson@gmail.com>
2013-02-07 22:00:07 +0100

    Copies data instead of using references

---
+++ example/exempel-tabelldata-OR.R
@@ -0,0 +1,29 @@
+require("reshape")
+require("data.table")
+require("Hierarchy")
+
+# .onLoad
+
+## Läs in data
+x <- read.csv2("example/exempel-tabelldata-OR.csv", fileEncoding = "Windows-1252")
+
+## Transformera
+x <- melt(x, id.vars = c("ID", "NAMN_SV", "FORMAN_SV"), variable_name = "YEAR")
+x$YEAR <- as.integer(substring(x$YEAR, 2, 5))
+
+# For each table
+
+## Selection
+fun <- function(x, path, forman, year) {
+    x <- x[x$FORMAN_SV %in% c(forman, "(...)") & x$YEAR == year, ]
+    x$FORMAN_SV <- forman
+    h <- path_enum$new(data = x, 
+                       path = "ID",
+                       metrics = "value")
+    ## Aggregation
+    tbl <- h$endnodes_aggregate(h$descendants_ids(path), function(x) sum(x, na.rm = TRUE))
+}
+
+tbl <- do.call("rbind", lapply(c("Premiepension", "Inkomstpension"), function(i) fun(x, "3", i, 2011)))
+
+cast(tbl, ID + NAMN_SV ~ FORMAN_SV, sum)
