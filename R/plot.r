54a46d82859fcbaca6c2a3a69aff07fbf3946baf
Thomas Reinholdsson <reinholdsson@gmail.com>
2013-02-13 21:46:14 +0100

    Update man

--- R/plot.r
+++ R/plot.r
@@ -31,7 +31,9 @@ fix_labels <- function(p) {
 #' @param dl_method directlabels method
 #' @param cex directlabels cex argument (font size)
 #' @param theme ggplot theme
-#' @example
+#' @examples
+#' require("ggplot2")
+#' require("directlabels")
 #' x <- structure(list(year = c(2010L, 2011L, 2012L, 2010L, 2011L, 2012L, 2010L, 2011L, 2012L), country = structure(c(3L, 3L, 3L, 2L, 2L, 2L, 1L, 1L, 1L), .Label = c("Finland", "Norway", "Sweden"), class = "factor"), value = c(2, 3, 3, 0, 4, 3, 1, 2, 2)), .Names = c("year", "country", "value"), row.names = c(NA, -9L), class = "data.frame")
 #' x$country <- paste("", x$country)
 #' line_plot(x, "year", "value", "country", xlab = "YEAR", ylab = "SEK")
