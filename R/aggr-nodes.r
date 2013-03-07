4f2cea838c3502100e59318b0a6c53a8cd1c35ac
Thomas Reinholdsson <reinholdsson@gmail.com>
2013-03-07 16:43:10 +0100

    Update aggr-nodes.r

--- R/aggr-nodes.r
+++ R/aggr-nodes.r
@@ -31,3 +31,34 @@ aggr_by <- function(data, path = colnames(data)[1],
 
     return(res)
 }
+
+
+# Aggregate by child
+
+aggr_by_child <- function(data, path = colnames(data)[1], metrics = "value", label, ids = "1", include = FALSE, fun = function(x) sum(x, na.rm = TRUE)) {
+
+  a <- Hierarchy:::path_enum$new(data = data, path = path, metrics = metrics)
+  ch <- a$children_ids(ids)
+
+  res <- do.call("rbind", lapply(ch, function(x) {
+
+    ds <- a$descendants_ids(x, include = include)
+
+    if (is.null(ds)) {
+      df <- a$aggregate(x, fun)
+    } else {
+      df <- a$aggregate(ds, fun)
+    }
+
+    if (include) df[df$label == a$node(x)[1, ][[label]], ][1, ][[label]] <- "(all)"
+
+    df$col_label <- a$node(x)[1, ][[label]]
+
+    # Replace part of id with *
+    .sep <- "\\."  # TODO: Improve
+    escaped_ids <- gsub("\\.", "\\\\.", ids)  # TODO: Improve
+    df[['Id']] <- gsub(sprintf("(^%1$s%2$s)(\\w+)(.*$)", escaped_ids, .sep),"\\1*\\3", df[['Id']])
+
+    return(df)
+  }))
+}
