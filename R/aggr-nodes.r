51a5f878bcf8dc97ad9763a84da48d54be650f8f
Thomas Reinholdsson <reinholdsson@gmail.com>
2013-03-07 09:48:32 +0100

    Add to_levels to aggr_by

--- R/aggr-nodes.r
+++ R/aggr-nodes.r
@@ -10,14 +10,25 @@
 #' @param by column to aggregate by
 #' @param fun aggregate function (e.g. sum)
 #' @param ... arguments passed to descendants_ids()
+#' @param to_levels if to convert ids to levels
 #' 
 #' @examples 
 #' # aggr_by(melt(notes), ids = "1.1.1.2.2", end = 2, include = TRUE)
 #' 
 #' @export
-aggr_by <- function(data, path = colnames(data)[1], metrics = "value", ids = "1", by = "variable", ..., fun = function(x) sum(x, na.rm = TRUE)) {
-    ddply(data, by, function(x) {
+aggr_by <- function(data, path = colnames(data)[1], 
+                    metrics = "value", 
+                    ids = "1", 
+                    by = "variable", ..., 
+                    fun = function(x) sum(x, na.rm = TRUE,
+                    to_levels = FALSE
+                    )) {
+    res <- ddply(data, by, function(x) {
         a <- Hierarchy:::path_enum$new(data = x, path = path, metrics = metrics)
         a$aggregate(a$descendants_ids(ids, ...), fun)
     })
+
+    if (to_levels) res[[path]] <- count_occ(res[[path]]
+
+    return(res)
 }
