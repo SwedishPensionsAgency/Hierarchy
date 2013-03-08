5fb303baadce0be858ce97d8ac852d6c225fa9bb
Thomas Reinholdsson <reinholdsson@gmail.com>
2013-03-08 10:46:45 +0100

    Tag version 0.1

--- example/plot.R
+++
@@ -1,6 +0,0 @@
-
-# Example data
-library(Hierarchy)
-x <- structure(list(year = c(2010L, 2011L, 2012L, 2010L, 2011L, 2012L, 2010L, 2011L, 2012L), country = structure(c(3L, 3L, 3L, 2L, 2L, 2L, 1L, 1L, 1L), .Label = c("Finland", "Norway", "Sweden"), class = "factor"), value = c(2, 3, 3, 0, 4, 3, 1, 2, 2)), .Names = c("year", "country", "value"), row.names = c(NA, -9L), class = "data.frame")
-x$country <- paste("", x$country)
-line_plot(x, "year", "value", "country", xlab = "YEAR", ylab = "SEK")
