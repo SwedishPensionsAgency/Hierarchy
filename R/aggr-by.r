44bcbf68b2bed09491eee880ba56070d8b54245f
Thomas Reinholdsson <reinholdsson@gmail.com>
2013-03-08 13:22:36 +0100

    Add id_format to aggr_by

--- R/aggr-by.r
+++ R/aggr-by.r
@@ -5,15 +5,15 @@
 #' 
 #' @param data data frame
 #' @param path column with path enumeration ids
-#' @param label label column (description of the variable); required.
+#' @param labels label columns (description of the variable).
 #' @param dims dimension columns
 #' @param metrics metric columns
 #' @param ids node id (e.g. "1.2.1.3")
 #' @param include if the node itself should be returned as a row
 #' @param fun function to be used in metric calculations (e.g. sum)
-#' @param to_levels if ids should be converted to levels
+#' @param id_format format of ids to "levels" or "stars"; default = "none".
 #' @param by_child if all calculations should be done on the children of the id node.
-#' Return column "root" will contain the label of the children.
+#' Return column "root" will contain the (first) label of the children.
 #' @param ... arguments passed to the descendants_ids() function; start = where to start in the subtree, end = where to end in the subtree.
 #' 
 #' @examples 
@@ -23,25 +23,25 @@
 # require(reshape2)
 # x <- read.table("data/notes.tab", sep = "\t", header = TRUE)
 # x <- melt(x)
-# ex1 <- aggr_by(x, ids = "1.1.1.2.2.1.3.1", path = "Id", label = "Label", dims = c("variable"), metrics = c("value"), include = FALSE, end = 1, by_child = FALSE)
-# ex2 <- aggr_by(x, ids = "1.1.1.2.2.1.3.1", path = "Id", label = "Label", dims = c("variable"), metrics = c("value"), include = TRUE, end = 1, by_child = TRUE)
-# ex3 <- aggr_by(x, ids = "1.1.1.2.2.1.3.1", path = "Id", dims = c("variable"), metrics = c("value"), include = TRUE, end = 2, label = "Label", by_child = TRUE, to_levels = TRUE)
+# ex1 <- aggr_by(x, ids = "1.1.1.2.2.1.3.1", path = "Id", labels = "Label", dims = c("variable"), metrics = c("value"), include = FALSE, end = 1, by_child = FALSE, id_format = "stars")
+# ex2 <- aggr_by(x, ids = "1.1.1.2.2.1.3.1", path = "Id", labels = "Label", dims = c("variable"), metrics = c("value"), include = TRUE, end = 1, by_child = TRUE, id_format = "stars")
+# ex3 <- aggr_by(x, ids = "1.1.1.2.2.1.3.1", path = "Id", dims = c("variable"), metrics = c("value"), include = TRUE, end = 2, labels = "Label", by_child = TRUE, id_format = "levels")
 #' 
 #' @export
 aggr_by <- function(data,
                     path = colnames(data)[1],
-                    label,
+                    labels = colnames(data)[2],
                     dims = NULL,
                     metrics = "value",
                     ids = "1",
                     include = FALSE,
                     fun = function(x) sum(x, na.rm = TRUE), 
-                    to_levels = FALSE,
+                    id_format = "none",
                     by_child = FALSE,
                     ...) {
 
     # Only keep variables of interest and aggregate on those
-    by <- c(path, label, dims)
+    by <- c(path, labels, dims)
     data <- subset(data, select = c(by, metrics))
     data <- ddply(data, by, colwise(fun))
 
@@ -58,12 +58,8 @@ aggr_by <- function(data,
         if (is.null(ds)) ds <- x
     
         df <- a$aggregate(ds, fun)
-        #if (include) df[df$label == a$node(x)[1, ][[label]], ][1, ][[label]] <- "(all)"
-    
-        df$root <- a$node(x)[1, ][[label]]
-    
-        #if (to_levels) df[[path]] <- id_to_levels(df[[path]])
-    
+        df$root <- a$node(x)[1, ][[labels[1]]]  # obs: the first label in labels is used
+
         return(df)
       }))
     }
@@ -77,11 +73,13 @@ aggr_by <- function(data,
     }
 
     # Replace part of id with *
-    .sep <- "\\."  # TODO: Improve
-    escaped_ids <- gsub("\\.", "\\\\.", ids)  # TODO: Improve
-    res[[path]] <- gsub(sprintf("(^%1$s%2$s)(\\w+)(.*$)", escaped_ids, .sep),"\\1*\\3", res[[path]])
+    if (id_format == "levels") {
+        res[[path]] <- id_to_levels(res[[path]])
+    } else if (id_format == "stars") {
+        .sep <- "\\."  # TODO: Improve
+        escaped_ids <- gsub("\\.", "\\\\.", ids)  # TODO: Improve
+        res[[path]] <- gsub(sprintf("(^%1$s%2$s)(\\w+)(.*$)", escaped_ids, .sep),"\\1*\\3", res[[path]])
+    }
         
-    if (to_levels) res[[path]] <- id_to_levels(res[[path]])
-    
     return(res)
 }
