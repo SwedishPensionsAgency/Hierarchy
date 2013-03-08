ebf31bccbde225f0d4e633720b4f5ce5c03902cd
Thomas Reinholdsson <reinholdsson@gmail.com>
2013-03-08 11:11:54 +0100

    Update aggr-by

--- R/aggr-by.r
+++ R/aggr-by.r
@@ -1,4 +1,4 @@
-#' Aggregate by
+#' Aggregate by variable
 #' 
 #' A wrapper function to the path enumeration class to aggregate nodes.
 #' The hierarchical data set must have a path enumerated column.
@@ -13,15 +13,16 @@
 #' @param to_levels if to convert ids to levels
 #' 
 #' @examples 
-#' # aggr_by(melt(notes), ids = "1.1.1.2.2", end = 2, include = TRUE)
+#' # aggr_by_var(melt(notes), ids = "1.1.1.2.2", end = 2, include = TRUE)
 #' 
 #' @export
-aggr_by <- function(data, path = colnames(data)[1], 
-                    metrics = "value", 
-                    ids = "1", 
-                    by = "variable", ..., 
-                    fun = function(x) sum(x, na.rm = TRUE),
-                    to_levels = FALSE) {
+aggr_by_var <- function(data, path = colnames(data)[1],
+                        ids = "1",
+                        metrics = "value",
+                        by = "variable",
+                        fun = function(x) sum(x, na.rm = TRUE),
+                        to_levels = FALSE,
+                        ...) {
     res <- ddply(data, by, function(x) {
         a <- Hierarchy:::path_enum$new(data = x, path = path, metrics = metrics)
         a$aggregate(a$descendants_ids(ids, ...), fun)
@@ -35,24 +36,30 @@ aggr_by <- function(data, path = colnames(data)[1],
 
 # Aggregate by child
 
-aggr_by_child <- function(data, path = colnames(data)[1], metrics = "value", label, ids = "1", include = FALSE, fun = function(x) sum(x, na.rm = TRUE), to_levels = FALSE) {
+aggr_by_child <- function(data,
+                          ids = "1",
+                          path = colnames(data)[1], 
+                          metrics = "value", 
+                          include = FALSE,
+                          fun = function(x) sum(x, na.rm = TRUE), 
+                          to_levels = FALSE,
+                          label = colnames(data)[1],
+                          ...) {
 
   a <- Hierarchy:::path_enum$new(data = data, path = path, metrics = metrics)
   ch <- a$children_ids(ids)
 
   res <- do.call("rbind", lapply(ch, function(x) {
 
-    ds <- a$descendants_ids(x, include = include)
+    ds <- a$descendants_ids(x, include = include, ...)
 
-    if (is.null(ds)) {
-      df <- a$aggregate(x, fun)
-    } else {
-      df <- a$aggregate(ds, fun)
-    }
+    if (is.null(ds)) ds <- x
 
+    df <- a$aggregate(ds, fun)
+    
     if (include) df[df$label == a$node(x)[1, ][[label]], ][1, ][[label]] <- "(all)"
 
-    df$col_label <- a$node(x)[1, ][[label]]
+    df$child <- a$node(x)[1, ][[label]]
 
     # Replace part of id with *
     .sep <- "\\."  # TODO: Improve
