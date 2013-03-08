1ad8967c138c0b04408623cf8357ae8670572162
Thomas Reinholdsson <reinholdsson@gmail.com>
2013-03-08 11:43:20 +0100

    Rewrite of aggr_by

--- R/aggr-by.r
+++ R/aggr-by.r
@@ -1,4 +1,4 @@
-#' Aggregate by variable
+#' Aggregate by variable and/or children
 #' 
 #' A wrapper function to the path enumeration class to aggregate nodes.
 #' The hierarchical data set must have a path enumerated column.
@@ -16,58 +16,65 @@
 #' # aggr_by_var(melt(notes), ids = "1.1.1.2.2", end = 2, include = TRUE)
 #' 
 #' @export
-aggr_by_var <- function(data, path = colnames(data)[1],
-                        ids = "1",
-                        metrics = "value",
-                        by = "variable",
-                        fun = function(x) sum(x, na.rm = TRUE),
-                        to_levels = FALSE,
-                        ...) {
-    res <- ddply(data, by, function(x) {
-        a <- Hierarchy:::path_enum$new(data = x, path = path, metrics = metrics)
-        a$aggregate(a$descendants_ids(ids, ...), fun)
-    })
-
-    if (to_levels) res[[path]] <- id_to_levels(res[[path]])
-
-    return(res)
-}
-
-
-# Aggregate by child
-
-aggr_by_child <- function(data,
+aggr_by <- function(data,
                           ids = "1",
                           path = colnames(data)[1], 
-                          metrics = "value", 
+                          metrics = "value",
+                          by = "variable",
                           include = FALSE,
                           fun = function(x) sum(x, na.rm = TRUE), 
                           to_levels = FALSE,
                           label = colnames(data)[1],
+                          child = FALSE,
                           ...) {
 
-  a <- Hierarchy:::path_enum$new(data = data, path = path, metrics = metrics)
-  ch <- a$children_ids(ids)
-
-  res <- do.call("rbind", lapply(ch, function(x) {
-
-    ds <- a$descendants_ids(x, include = include, ...)
-
-    if (is.null(ds)) ds <- x
-
-    df <- a$aggregate(ds, fun)
     
-    if (include) df[df$label == a$node(x)[1, ][[label]], ][1, ][[label]] <- "(all)"
-
-    df$child <- a$node(x)[1, ][[label]]
+        #browser()
+    aggr_each <- function(x) {
+      a <- Hierarchy:::path_enum$new(data = x, path = path, metrics = metrics)
+      
+      if (child) {
+        ids <- a$children_ids(ids)
+      }
+    
+      res <- do.call("rbind", lapply(ids, function(x) {
+        ds <- a$descendants_ids(x, include = include, ...)
+        if (is.null(ds)) ds <- x
+    
+        df <- a$aggregate(ds, fun)
+        if (include) df[df$label == a$node(x)[1, ][[label]], ][1, ][[label]] <- "(all)"
+    
+        df$root <- a$node(x)[1, ][[label]]
+    
+        #if (to_levels) df[[path]] <- id_to_levels(df[[path]])
+    
+        return(df)
+      }))
+    }
+    
+    if (is.null(by)) {
+        res <- aggr_each(data)
+    } else {
+    res <- ddply(data, by, function(x) {
+        aggr_each(x)
+    })
+    }
 
     # Replace part of id with *
     .sep <- "\\."  # TODO: Improve
     escaped_ids <- gsub("\\.", "\\\\.", ids)  # TODO: Improve
-    df[['Id']] <- gsub(sprintf("(^%1$s%2$s)(\\w+)(.*$)", escaped_ids, .sep),"\\1*\\3", df[['Id']])
+    res[[path]] <- gsub(sprintf("(^%1$s%2$s)(\\w+)(.*$)", escaped_ids, .sep),"\\1*\\3", res[[path]])
+        
+    if (to_levels) res[[path]] <- id_to_levels(res[[path]])
     
-    if (to_levels) df[[path]] <- id_to_levels(df[[path]])
-
-    return(df)
-  }))
+    return(res)
 }
+
+### Example
+# require(Hierarchy)
+# require(plyr)
+# require(reshape2)
+# x <- read.table("data/notes.tab", sep = "\t", header = TRUE)
+# x <- melt(x)
+# test <- aggr_by_child(x, ids = "2.1.1", path = "Id", by = "Id", include = FALSE, end = 2)
+# head(test)
