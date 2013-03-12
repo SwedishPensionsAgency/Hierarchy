359c516e4fde100af733f6baf34cd78980def9a1
Thomas Reinholdsson <reinholdsson@gmail.com>
2013-03-12 20:27:43 +0100

    Bug fix: Imports -> Depends

--- R/Hierarchy.r
+++ R/Hierarchy.r
@@ -3,7 +3,6 @@
 #' The Hierarchy package aims to simplify the work with hierarchical data structures in R. 
 #' It is inspired by Ancestry - a Ruby and Rails gem/plugin by Stefan Henzen.
 #' 
-#' @import plyr reshape2
 #' @docType package
 #' @name Hierarchy
 NULL
