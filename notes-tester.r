2a5e923283d4229dd82536cdf7ff412ccc22ffa7
Thomas Reinholdsson <reinholdsson@gmail.com>
2013-03-17 22:41:23 +0100

    Update test code

--- notes-tester.r
+++ notes-tester.r
@@ -3,13 +3,17 @@ require(plyr)
 require(data.table)
 
 .year <- 2011
+.lang <- "sv"
 .grand_label <- "Summa"
 
-notes <- read.csv2("~/Desktop/noter.csv", stringsAsFactors = FALSE)
-notes <- as.data.table(melt(notes))
-notes$year <- substring(notes$variable, 2, 5)
-notes$notes <- notes$Notes_sv
-notes$label <- notes$Label_sv
+import_csv <- function(file, lang) {
+    x <- read.csv2(file, stringsAsFactors = FALSE)
+    x <- as.data.table(melt(x))
+    x$year <- substring(x$variable, 2, 5)
+    x$label <- x[[paste("Label", lang, sep = "_")]]
+    x$notes <- x[[paste("Notes", lang, sep = "_")]]
+    return(x)
+}
 
 custom_aggr_by <- function(data,
                            path = "Id", 
@@ -30,12 +34,12 @@ custom_aggr_by <- function(data,
             ...)
 }
 
-by_child_table <- function(id, fun = sum_aggr_na) {
-    this_year <- custom_aggr_by(notes[year == .year], ids = id, by_child = TRUE, to_levels = FALSE, fun = fun)
+by_child_table <- function(data, id, fun = sum_aggr_na) {
+    this_year <- custom_aggr_by(data[year == .year], ids = id, by_child = TRUE, to_levels = FALSE, fun = fun)
     colnames(this_year)[ncol(this_year)] <- paste(.grand_label, .year)
     sort_key <- 1:nrow(this_year)
     
-    prev_year <- custom_aggr_by(notes[year == .year - 1], ids = id, by_child = TRUE, to_levels = FALSE, fun = fun)
+    prev_year <- custom_aggr_by(data[year == .year - 1], ids = id, by_child = TRUE, to_levels = FALSE, fun = fun)
     colnames(prev_year)[ncol(prev_year)] <- paste(.grand_label, .year - 1)
     
     x <- merge(this_year, prev_year[, c(1:3, ncol(prev_year))], by.x = c("Id", "label", "notes"), all = FALSE, sort = FALSE)
@@ -46,28 +50,37 @@ by_child_table <- function(id, fun = sum_aggr_na) {
 ### NOTER ###
 
 # IP, resultaträkning
-custom_aggr_by(notes, ids = "1.1.1.2.1.2", end = 2, margins = NULL, grand_label = "Årets resultat")
+x <- import_csv("~/Desktop/noter.csv", .lang)
+x <- custom_aggr_by(x, ids = "1.1.1.2.1.2", end = 2, margins = NULL, grand_label = "Årets resultat")
 
 # IP, balansräkning
-custom_aggr_by(notes, ids = "1.1.1", end = 3, include = FALSE, margins = NULL)
+x <- import_csv("~/Desktop/noter.csv", .lang)
+x <- custom_aggr_by(x, ids = "1.1.1", end = 3, include = FALSE, margins = NULL)
 
 # PP, resultaträkning
-custom_aggr_by(notes, ids = "1.2.1.2.1.2", end = 2, margins = NULL, grand_label = "Årets resultat")
+x <- import_csv("~/Desktop/noter.csv", .lang)
+x <- custom_aggr_by(x, ids = "1.2.1.2.1.2", end = 2, margins = NULL, grand_label = "Årets resultat")
 
 # PP, balansräkning
-custom_aggr_by(notes, ids = "1.2.1", end = 3, include = FALSE, margins = NULL)
+x <- import_csv("~/Desktop/noter.csv", .lang)
+x <- custom_aggr_by(x, ids = "1.2.1", end = 3, include = FALSE, margins = NULL)
 
 # Not 2
-custom_aggr_by(notes, ids = "1.1.1.2.1.2.1.2", margins = NULL, fun = function(x) sum_aggr_na(-x))
+x <- import_csv("~/Desktop/noter.csv", .lang)
+x <- custom_aggr_by(x, ids = "1.1.1.2.1.2.1.2", margins = NULL, fun = function(x) sum_aggr_na(-x))
 
 # Not 3
-by_child_table("1.1.1.2.1.2.1.3.1")
+x <- import_csv("~/Desktop/noter.csv", .lang)
+x <- by_child_table(x, "1.1.1.2.1.2.1.3.1")
 
 # Not 4
-custom_aggr_by(notes, ids = "1.1.1.2.1.2.1.4", margins = NULL, fun = function(x) sum_aggr_na(-x))
+x <- import_csv("~/Desktop/noter.csv", .lang)
+x <- custom_aggr_by(x, ids = "1.1.1.2.1.2.1.4", margins = NULL, fun = function(x) sum_aggr_na(-x))
 
 # Not 7
-custom_aggr_by(notes, ids = "1.1.1.2.1.2.3.6", margins = NULL, fun = function(x) sum_aggr_na(-x))
+x <- import_csv("~/Desktop/noter.csv", .lang)
+x <- custom_aggr_by(x, ids = "1.1.1.2.1.2.3.6", margins = NULL, fun = function(x) sum_aggr_na(-x))
 
 # Not 8
-by_child_table("1.1.1.2.1.2.3.7", fun = function(x) sum_aggr_na(-x))
+x <- import_csv("~/Desktop/noter.csv", .lang)
+x <- by_child_table(x, "1.1.1.2.1.2.3.7", fun = function(x) sum_aggr_na(-x))
